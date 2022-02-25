2d7a8e8dd1f5b6dc603899e99222e4bd54245837
tsz <???@163.com>
2022-02-25 16:39:48 +0800

    perf: delete needless import
    delete needless import

--- src/main/java/priv/zg/controller/UsersController.java
+++ src/main/java/priv/zg/controller/UsersController.java
@@ -3,7 +3,6 @@ package priv.zg.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,7 +14,6 @@ import priv.zg.domain.Users;
 import priv.zg.service.UsersService;
 import priv.zg.util.CheckUtils;
 
-import java.util.List;
 
 /**
  * <p>
